eda66657956c4daddccceef9565bfcd478978ba1
andoyako <134358065+AndrewOrbasido@users.noreply.github.com>
2024-11-08 18:06:44 +0800

    assignment 1 done

---
+++ src/assignments/assignments.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Param, Get } from '@nestjs/common';
+
+@Controller('assignments')
+export class AssignmentsController {
+    @Get('fibonacci/:n')
+    getFibonacci(@Param('n') n: string): number[]{
+        const num = parseInt(n);
+        if (isNaN(num) || num < 0){
+            throw new Error('Error: Enter a valid number (non-negative)');
+            }
+            return this.calculateFibonacci(num);
+        }
+
+        private calculateFibonacci(n: number): number[]{
+            const fibonacci: number[] = [];
+            let a = 0, b =  1;
+
+            for (let i = 0; i < n; i++){
+                fibonacci.push(a);
+                const next = a + b;
+                a = b;
+                b = next;
+            }
+            return fibonacci;
+        }
+
+}
